3f97034d161f0ff8d9b6e56394c7367d75521b01
dinesh.kumar <dinesh67.kumar@gmail.com>
2020-03-21 20:58:59 +0530

    first commit

---
+++ imbackendapp/src/main/java/com/dinesh/imbackendapp/ImbackendappApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.imbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImbackendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImbackendappApplication.class, args);
+	}
+
+}
---
+++ imbackendapp/src/test/java/com/dinesh/imbackendapp/ImbackendappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.imbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImbackendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
